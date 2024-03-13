1294049f8dbef5a416135692ea8145ee72500083
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2024-03-13 21:19:49 +0330

    firstt commit

---
+++ Controllers/center.cs
@@ -0,0 +1,115 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class center : Controller
+{
+   
+
+
+ 
+   public IActionResult index2() 
+   {
+    return View();
+   }
+
+
+ public IActionResult academicsdetails() 
+   {
+    return View();
+   }
+
+    public IActionResult academics() 
+   {
+    return View();
+   }
+
+    public IActionResult alumni() 
+   {
+    return View();
+   }
+
+    public IActionResult campuslife() 
+   {
+    return View();
+   }
+
+    public IActionResult comingsoon() 
+   {
+    return View();
+   }
+
+    public IActionResult contactus() 
+   {
+    return View();
+   }
+
+    public IActionResult courses() 
+   {
+    return View();
+   }
+
+    public IActionResult coursesdetails() 
+   {
+    return View();
+   }
+
+    public IActionResult events() 
+   {
+    return View();
+   }
+
+    public IActionResult eventsdetails() 
+   {
+    return View();
+   }
+
+   public IActionResult faq() 
+   {
+    return View();
+   }
+
+   public IActionResult graduateadmission() 
+   {
+    return View();
+   }
+
+   public IActionResult healthcare() 
+   {
+    return View();
+   }
+
+   public IActionResult healthcaredetails() 
+   {
+    return View();
+   }
+
+   public IActionResult latestnews() 
+   {
+    return View();
+   }
+
+   public IActionResult login() 
+   {
+    return View();
+   }
+
+   public IActionResult newsdetails() 
+   {
+    return View();
+   }
+
+   public IActionResult recoverpassword() 
+   {
+    return View();
+   }
+
+    public IActionResult register() 
+   {
+    return View();
+   }
+
+    public IActionResult termsconditions() 
+   {
+    return View();
+   }
+
+}
